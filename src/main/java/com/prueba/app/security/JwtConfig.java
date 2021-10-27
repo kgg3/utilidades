6e6404c5d4b21d7a374ed8d4875a0125f14e544b
kgg3 <kevlar_garnet@hotmail.com>
2021-10-27 13:29:27 +0200

    Delete src directory

--- src/main/java/com/prueba/app/security/JwtConfig.java
+++
@@ -1,5 +0,0 @@
-package com.prueba.app.security;
-
-public class JwtConfig {
-
-}
--- src/main/java/com/prueba/app/service/IUsuarioService.java
+++
@@ -1,9 +0,0 @@
-package com.prueba.app.service;
-
-import com.prueba.app.entities.UsuarioEntity;
-
-public interface IUsuarioService {
-	
-	public UsuarioEntity getUsuario(String username, String password);
-	
-}
